32b3447bb97986f5c10261cfbb85cd0743705197
Trent008 <122338142+Trent008@users.noreply.github.com>
2023-05-12 16:05:55 -0700

    fix bugs

--- src/main/include/Parameters.h
+++ src/main/include/Parameters.h
@@ -5,14 +5,14 @@
 // parameters for robot movement and autonomous
 struct Parameters
 {  
-    double static const robotAccelMetersPerSecondSquared = 8; // acceleration rate of the robot pose on the field
-    double static const ampsForRobotAccel = 40;
-    double static const wheelDiameter = 3.9;
-    double static const driveMotorInchesPerRotation = (M_PI * wheelDiameter / 6.75);
-    double static const falconMaxRotationsPerSecond = 101;
+    double const robotAccelMetersPerSecondSquared = 8; // acceleration rate of the robot pose on the field
+    double const ampsForRobotAccel = 40;
+    double const wheelDiameter = 3.9;
+    double const driveMotorInchesPerRotation = (M_PI * wheelDiameter / 6.75);
+    double const falconMaxRotationsPerSecond = 101;
     double const swerveMaxRotationRate = falconMaxRotationsPerSecond * driveMotorInchesPerRotation / std::hypot(17.75, 25) * 180 / M_PI;
-    double static const maxRobotMetersPerSecond = falconMaxRotationsPerSecond * driveMotorInchesPerRotation * 0.0254;
-    double static const robotPercentChangePerCycle = (robotAccelMetersPerSecondSquared/maxRobotMetersPerSecond/50);
+    double const maxRobotMetersPerSecond = falconMaxRotationsPerSecond * driveMotorInchesPerRotation * 0.0254;
+    double const robotPercentChangePerCycle = (robotAccelMetersPerSecondSquared/maxRobotMetersPerSecond/50);
     double const defaultAutoMaxDriveRate = 0.2;
     double const defaultAutoMaxRotationRate = 0.2;
 
